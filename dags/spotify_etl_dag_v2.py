d42633457750188d6df73d83d8e52b2355591642
Sid <sidchavan64@gmail.com>
2025-04-15 22:01:49 -0400

    artists extraction script updated with search, insert and load

--- dags/spotify_etl_dag_v2.py
+++ dags/spotify_etl_dag_v2.py
@@ -9,6 +9,8 @@ def run_spotify_etl():
                     "Billie Eilish", "SZA", "Travis Scott", "Dua Lipa", "Post Malone"]
     
     artist_ids = load_artists(conn, token, artist_names)
-    album_ids = load_albums(conn, token, artist_ids)
+    # album_ids = load_albums(conn, token, artist_ids)
+
+    #TODO: now we use the artist_ids here to pass it to albums or other modules based on what the API needs
 
     conn.close()
--- scripts/extract/artists.py
+++ scripts/extract/artists.py
@@ -1,5 +1,50 @@
+import json
+import logging 
+from utils.common_utils import get_token, get_connection, safe_request, auth_header
 
+# Extract artist object from name
+def search_artist(artist_name, token):
+    url = "https://api.spotify.com/v1/search"
+    params = {"q": artist_name, "type": "artist", "limit": 1}
+    data = safe_request(url, auth_header(token), params)
+    items = data.get("artists", {}).get("items",[])
+    return items[0] if items else None
 
+# Insert our artist data into staging tables
+# currently using on conflict do nothing to deal with duplicates
+def insert_stg_artist(conn, artist):
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO stg_artists (artist_id, name, followers, genres, popularity, external_url, image_url, raw_json)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (artist_id) DO NOTHING
+        """, (
+            artist["id"],
+            artist["name"],
+            artist["followers"]["total"],
+            artist["genres"],
+            artist["popularity"],
+            artist["external_urls"].get("spotify"),
+            artist["images"][0]["url"] if artist.get("images") else None,
+            json.dumps(artist)
+        ))
+        conn.commit()
+
+# ETL logic for artist only
 def load_artists(conn, token, artist_names):
-    return
-    # extract from apotify api and insert into stg_artists
+    artist_ids = []
+
+    for name in artist_names:
+        try:
+            logging.info(f"Searching artist: {name}")
+            artist = search_artist(name, token)
+            if not artist:
+                logging.warning(f"No artist found: {name}")
+                continue
+
+            insert_stg_artist(conn, artist)
+            artist_ids.append(artist["id"])
+        except Exception as e:
+            logging.error(f"Error processing {name}: {e}")
+
+    return artist_ids
