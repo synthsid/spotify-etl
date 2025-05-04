dbe7a44d59ff345821a08cb8e75b4ad7fbb0febb
Sid <sidchavan64@gmail.com>
2025-05-03 21:45:15 -0400

    album stats aggregate table

--- scripts/standalone/aggregate_query_ideas.sql
+++ scripts/standalone/aggregate_query_ideas.sql
@@ -15,5 +15,19 @@ GROUP BY da.artist_key, da.artist_name;
 
 
 -- album stats
+-- Number of tracks, total duration, and average duration per album
+
+CREATE TABLE agg_album_stats AS
+SELECT
+    da.album_id,
+    da.album_name,
+    COUNT(ft.track_id) AS num_tracks,
+    SUM(ft.duration_ms) AS total_duration_ms,
+    AVG(ft.duration_ms) AS avg_duration_ms
+FROM fact_track ft
+JOIN dim_album da ON ft.album_id = da.album_id
+GROUP BY da.album_id, da.album_name;
+
+
 -- artist popularity?
 
