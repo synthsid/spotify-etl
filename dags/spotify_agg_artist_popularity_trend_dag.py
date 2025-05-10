4a1b6bd91b296a5219b97d9afb22aa35b8cb059b
Sid <sidchavan64@gmail.com>
2025-05-10 16:30:27 -0400

    dag for popularity_trend wait for fact table

--- dags/spotify_agg_artist_popularity_trend_dag.py
+++ dags/spotify_agg_artist_popularity_trend_dag.py
@@ -20,3 +20,24 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=3),
 }
+
+with DAG(
+    dag_id="spotify_agg_artist_popularity_trend",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    wait_for_transform_fact_track = ExternalTaskSensor(
+        task_id="wait_for_transform_fact_track",
+        external_dag_id="spotify_transform",
+        external_task_id="transform_fact_track",
+        mode="poke",
+        timeout=600,
+        poke_interval=30,
+    )
+
+    aggregate = PythonOperator(
+        task_id="aggregate_artist_popularity_trend",
+        python_callable=run_agg_artist_popularity,
+    )
