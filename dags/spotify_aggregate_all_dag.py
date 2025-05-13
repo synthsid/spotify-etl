ca4cd3cc21f703c6bb6d9993a1b5fd09b5ec8fac
Sid <sidchavan64@gmail.com>
2025-05-12 22:11:27 -0400

    all aggregation dag script

---
+++ dags/spotify_aggregate_all_dag.py
@@ -0,0 +1,11 @@
+# TODO: Need to put all the agregate script in one
+
+
+# def run_agg_total_tracks_by_artist():
+
+# def run_agg_album_stats():
+
+# def run_agg_artist_popularity():
+
+
+# wait_for_transform_fact_track >> [agg_tracks_by_artist, agg_album_stats, agg_artist_popularity]
