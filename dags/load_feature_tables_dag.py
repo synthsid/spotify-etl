c513803eb9d80593a4cf9f8df174873164842922
Sid <sidchavan64@gmail.com>
2025-05-25 21:41:02 -0400

    transformation dag for feature tables

--- dags/load_feature_tables_dag.py
+++ dags/load_feature_tables_dag.py
@@ -17,4 +17,30 @@ with DAG(
     description="Transform and load audio features, artist genres, and track markets",
 ) as dag:
     
-    # Load the new tables 
+
+    load_dim_audio_feature = PostgresOperator(
+        task_id="load_dim_audio_feature",
+        postgres_conn_id="spotify_pg",
+        sql="/opt/airflow/sql/transform/dim_audio_feature.sql"
+    )
+
+    load_dim_genre = PostgresOperator(
+        task_id="load_dim_genre",
+        postgres_conn_id="spotify_pg",
+        sql="/opt/airflow/sql/transform/dim_genre.sql"
+    )
+
+    load_fact_artist_genre = PostgresOperator(
+        task_id="load_fact_artist_genre",
+        postgres_conn_id="spotify_pg",
+        sql="/opt/airflow/sql/transform/fact_artist_genre.sql"
+    )
+
+    load_fact_track_market = PostgresOperator(
+        task_id="load_fact_track_market",
+        postgres_conn_id="spotify_pg",
+        sql="/opt/airflow/sql/transform/fact_track_market.sql"
+    )
+
+    load_dim_audio_feature >> load_dim_genre >> load_fact_artist_genre
+    load_dim_audio_feature >> load_fact_track_market
