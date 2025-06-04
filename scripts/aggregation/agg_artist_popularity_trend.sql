9e96529c3c9062dba2f796bb69acd4d23839d581
Sid <sidchavan64@gmail.com>
2025-06-03 23:02:49 -0400

    fixing naming convention for aggregate table

--- scripts/aggregation/agg_artist_popularity_trend.sql
+++ scripts/aggregation/agg_artist_popularity_trend.sql
@@ -4,12 +4,12 @@
 CREATE TABLE IF NOT EXISTS agg_artist_popularity_trend AS
 SELECT
     da.artist_key,
-    da.artist_name,
-    fap.snapshot_date,
-    AVG(fap.popularity) AS avg_popularity
-FROM fact_artist_popularity fap
+    da.name as artist_name,
+    ap.snapshot_date,
+    AVG(ap.popularity) AS avg_popularity
+FROM fact_artist_popularity ap
 JOIN dim_artist da
-  ON fap.artist_id = da.artist_id
+  ON ap.artist_key = da.artist_key
 WHERE da.is_current = TRUE
-GROUP BY da.artist_key, da.artist_name, fap.snapshot_date
-ORDER BY da.artist_key, fap.snapshot_date;
+GROUP BY da.artist_key, da.name, ap.snapshot_date
+ORDER BY da.artist_key, ap.snapshot_date;
--- scripts/aggregation/agg_total_tracks_by_artist.sql
+++ scripts/aggregation/agg_total_tracks_by_artist.sql
@@ -4,14 +4,15 @@
 CREATE TABLE IF NOT EXISTS agg_total_tracks_by_artist AS
 SELECT
     da.artist_key,
-    da.artist_name,
+    da.name as artist_name,
     COUNT(ft.track_id) AS total_tracks
 FROM fact_track ft
-JOIN dim_album d_al ON ft.album_key = d_al.album_key
-JOIN dim_artist da ON d_al.artist_key = da.artist_key
+JOIN dim_album al ON ft.album_key = al.album_key
+JOIN dim_artist da ON al.artist_id= da.artist_id
 WHERE da.end_date IS NULL  -- Only include current artist records
-  AND d_al.end_date IS NULL  -- Only include current album records
-GROUP BY da.artist_key, da.artist_name;
+  AND al.end_date IS NULL  -- Only include current album records
+GROUP BY da.artist_key, da.name;
+
 
 
 -- ToDo: make a version that incrementally updates the table using INSERT INTO instead of recreating the table
